4c879984b5ce723f917cd4b2b5c4d843d84745aa
R&D Automatic Backup <backup@localhost>
2026-09-12 13:58:51 +0530

    Exact verified backup batch 6 - 2026-09-12 13:58:51

--- R&D Projects/iAWS Development/iAWS Mega Mini/iAWS_Automatic_Weighing_System_v2.1_NEW_LOGO_ICON_BRANDING_FIXED/iAWS_Automatic_Weighing_System_v2.1_EXACT_DRAWING_UI_NEW_LOGO_BRANDING/src/RfidVehicleAccess.App/Services/SystemEventHub.cs
+++
@@ -1,9 +0,0 @@
-using RfidVehicleAccess.Models;
-
-namespace RfidVehicleAccess.Services;
-
-public sealed class SystemEventHub
-{
-    public event EventHandler<AppLogEntry>? LogAdded;
-    public void PublishLog(AppLogEntry entry) => LogAdded?.Invoke(this, entry);
-}
