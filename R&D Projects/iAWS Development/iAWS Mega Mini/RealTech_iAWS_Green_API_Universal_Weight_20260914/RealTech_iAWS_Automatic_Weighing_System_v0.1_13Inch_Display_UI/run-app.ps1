43fe15c537ffd98837b09ecd839f92ac0f9e4997
R&D Automatic Backup <backup@localhost>
2026-09-15 10:15:53 +0530

    Exact verified backup batch 17 - 2026-09-15 10:15:52

--- R&D Projects/iAWS Development/iAWS Mega Mini/RealTech_iAWS_Green_API_Universal_Weight_20260914/RealTech_iAWS_Automatic_Weighing_System_v0.1_13Inch_Display_UI/run-app.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$project = Join-Path $PSScriptRoot "src\RfidVehicleAccess.App\RfidVehicleAccess.App.csproj"
-
-dotnet restore $project
-dotnet run --project $project
